b3cc77bc1ac15dab1d88afe03f248df8162fd01f
thug-creator <mr.pup06@mail.ru>
2025-11-15 10:29:34 +0300

    Update forms.py

--- testdrive/myapp/forms.py
+++ testdrive/myapp/forms.py
@@ -7,4 +7,5 @@ class UserForm(forms.Form):
     phone = forms.CharField(label='Телефон')
     email = forms.EmailField(label='Почта')
     car = forms.ChoiceField(label='Авто', choices=CARS)
+    datatime = forms.DateTimeField(label='Дата и время', widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
     coment = forms.CharField(label='Комментарий', widget=forms.Textarea)
